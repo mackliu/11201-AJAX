6af2c0c525e0976a86fd3057dd9242d58aecb000
mackliu <macklun@gmail.com>
2023-07-10 15:28:44 +0800

    以ajax 的方式製作新增資料功能

--- app2/api/insert.php
+++ app2/api/insert.php
@@ -1,5 +1,8 @@
 <?php
 //處理新增資料的請求
-
-
+$pdo=new PDO('mysql:host=localhost;charset=utf8;dbname=ajax_student','root','');
+//$pdo=new PDO('mysql:host=localhost;charset=utf8;dbname=s1120226','s1120226','s1120226');
+$keys=array_keys($_POST);
+echo $pdo->exec("insert into students (`".join("`,`",$keys)."`) 
+                        value('".join("','",$_POST)."')");
 ?>
